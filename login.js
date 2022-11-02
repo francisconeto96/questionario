9a72187b30ae636f220b9284d70e9067fabda253
Francisco Neto <98923737+francisconeto96@users.noreply.github.com>
2022-11-02 18:42:29 -0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+    $("#entrar1").submit(function(e){
+        e.preventDefault();
+
+        console.log("funcionando");
+        
+
+        const firebaseConfig = {
+            apiKey: "AIzaSyBJgX1Rgm9LqVVAOoyjsniCA-G--JRozqg",
+            authDomain: "cadastro-e151e.firebaseapp.com",
+            projectId: "cadastro-e151e",
+            storageBucket: "cadastro-e151e.appspot.com",
+            messagingSenderId: "480379320401",
+            appId: "1:480379320401:web:63372de1a43314ad21a070",
+            measurementId: "G-2S62HYV0NS"
+          };
+
+        
+          firebase.initializeApp(firebaseConfig)
+    const db = firebase.firestore()
+    let auth = firebase.auth()
+    
+    function login() {
+        let userEmail = $("#email").val();
+        let userPassword = $("#senha").val();
+
+        auth.signInWithEmailAndPassword(userEmail,userPassword)
+            .then(loggedser => {
+            console.log(auth.currentUser);
+
+            alert("Login realizado com sucesso!");
+            location.href = "http://www.devmedia.com.br";
+
+        }).catch(error => {
+            console.log(error)
+
+            alert("Erro ao efetuar o login!");
+        })
+    }
+    login();
+    
+
+        
+    })
+    
+    });  
